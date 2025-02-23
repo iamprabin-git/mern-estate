8fdcb3c58af31e2b61289194482ca42650120d4d
prabin dangol <dprabin23@gmail.com>
2025-02-23 06:47:17 +0545

    create a test api
    Signed-off-by: prabin dangol <dprabin23@gmail.com>

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
+import userRouter from './routes/user.route.js';
 
 mongoose
   .connect(process.env.MONGO)
@@ -13,7 +14,11 @@ mongoose
   });
 
 const app= express();
+app.use(express.json());
 
 app.listen(3000, () => {
 console.log('server is running in port 3000')
 });
+
+app.use('/api/user', userRouter);
+
