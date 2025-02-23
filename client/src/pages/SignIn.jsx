05da850a960f554174bc668992ef979138b7c545
prabin dangol <dprabin23@gmail.com>
2025-02-23 23:51:08 +0545

    create and add OAuth Component
    Signed-off-by: prabin dangol <dprabin23@gmail.com>

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { signInStart, SignInSuccess, signInFailure } from '../redux/user/userSlice';
+import OAuth from '../components/OAuth';
 
 export default function Signin() {
   const [formData, setFormData] = useState({});
@@ -45,6 +46,7 @@ export default function Signin() {
         <input type="text" placeholder='Email ' className='border p-3 rounded-lg' id='email' onChange={handleChange} />
         <input type="text" placeholder='Password ' className='border p-3 rounded-lg' id='password' onChange={handleChange} />
         <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading....' : 'Sign in'}</button>
+        <OAuth/>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Don't Have an Account?</p>
