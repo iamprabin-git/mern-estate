c673f9c98ba2f63dda586c13679f5c93fb91e9e6
prabin dangol <dprabin23@gmail.com>
2025-02-25 22:02:49 +0545

    add sign out functionality
    Signed-off-by: prabin dangol <dprabin23@gmail.com>

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -14,7 +14,7 @@ import {
   deleteUserFailure,
   deleteUserStart,
   deleteUserSuccess,
-  // signOutUserStart,
+  signOutUserStart,
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
@@ -112,6 +112,21 @@ export default function Profile() {
     }
   };
 
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch('/api/auth/signout');
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(data.message));
+    }
+  };
+
   return (
     <div>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -176,7 +191,9 @@ export default function Profile() {
         className='text-red-700 cursor-pointer'>
           Delete account
         </span>
-        <span className='text-red-700 cursor-pointer'>
+        <span 
+        onClick={handleSignOut}
+        className='text-red-700 cursor-pointer'>
           Sign out
         </span>
       </div>
