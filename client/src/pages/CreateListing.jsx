8b18d9e714ed68cb5a6168ccb1a3c7e8ef7f6763
prabin dangol <dprabin23@gmail.com>
2025-02-26 22:59:52 +0545

    update create listing
    Signed-off-by: prabin dangol <dprabin23@gmail.com>

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -136,6 +136,7 @@ export default function CreateListing() {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
         },
         body: JSON.stringify({
           ...formData,
